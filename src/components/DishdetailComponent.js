eb2ac2ff49afe1b4d68e84f33d715e2092f0db3f
Ajit Kumar Singh <64456421+ajitkrsingh0786@users.noreply.github.com>
2020-08-09 09:35:11 +0530

    10th commit: SPA part-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,9 @@
 import React,{Component} from 'react';
-import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from  'reactstrap';
+//import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from  'reactstrap';
 //import {DishDetail} from './DishdetailComponent';
+import { Card, CardImg, CardText, CardBody,
+   CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
  
      
@@ -11,7 +14,7 @@ import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from  'reactstr
 
             return(
                   <CardBody>
-                     <CardTitle >heading>{dish.name}</CardTitle>
+                     <CardTitle>heading>{dish.name}</CardTitle>
                      <CardText> {dish.description }</CardText>
                      </CardBody>
                       );
@@ -45,23 +48,34 @@ import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from  'reactstr
 
      const DishDetail = (props) => {
              
-           if(props.dish!=null){
+           if(props.dish!=null && props.comments!=null){
 
-          const comments=props.dish.comments.map(RenderComments);
+            const comments=props.comments.map(RenderComments);
 
                  return(
                       <div className="container">
+                          <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
                        <div div className="row">
-                     <div className="col-12 col-md-5 mt-1">
+                           <div className="col-12 col-md-5 mt-1">
                          
-                    <Card >
-                    <CardImg width="100%" object src={props.dish.image} alt={props.dish.name}/> 
+                           <Card >
+                            <CardImg width="100%" object src={props.dish.image} alt={props.dish.name}/> 
                           
-                         <RenderDish dish={props.dish} />
-                         </Card>
+                               <RenderDish dish={props.dish} />
+                               </Card>
                           </div>
                             
-                            <div className="col-12 col-md-5 mt-1">
+                         <div className="col-12 col-md-5 mt-1">
                            <h4>Comments:</h4>
                                {comments}
                             </div>
