c6603e8c2798149408c5c451d9a9b7f333bb8d90
Ajit Kumar Singh <64456421+ajitkrsingh0786@users.noreply.github.com>
2020-09-23 17:09:24 +0530

    27th commit: assignment 4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
+import { Loading } from './LoadingComponent';
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
-            <RenderLeader leaders={leader}/>
+            
+            <Stagger in>
+            <RenderLeader leaders={leader} isLoading={props.leaderLoading} errMess={props.leaderErrMess}/>
+            </Stagger>
+ 
         );
     });
 
     function RenderLeader({leaders}) {
             return(
-             <div key={leaders.id} className="col-12 mt-5">
-                 <Media tag="li">
-                     <Media left middle>
-                         <Media object src={leaders.image} alt={leaders.name}/>               
-                     </Media>
-                     <Media body classNAme="ml-5">
-                          <Media heading> {leaders.name} </Media>
-                           <p>{leaders.designation}</p>
-                          < p>{leaders.description}</p>
-                     </Media>
-                 </Media>
-             </div>
+                <Fade in >
+                <Media tag="li">
+                    <Media left middle>
+                        <Media object src={baseUrl + leaders.image} alt={leaders.name} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{leaders.name}</Media>
+                        <Media body className="mb-3">{leaders.designation}</Media>
+                        <p>{leaders.description}</p>
+                    </Media>
+                </Media>
+                </Fade>   
             )
         
     }
